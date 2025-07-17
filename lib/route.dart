fa7ee35b23bd4d3c640010209b414eae664bb3b2
alfayej55 <mdalfayej017fa@gmail.com>
2025-07-17 11:44:10 +0600

    fast

---
+++ lib/route.dart
@@ -0,0 +1,17 @@
+import 'package:class1/splashpage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+
+class AppRoute{
+  static String splasScreen='/splashscreen';
+
+
+
+ static List<GetPage> pages =[
+    GetPage(name: splasScreen, page:()=>Splashpage())
+
+  ];
+
+}
